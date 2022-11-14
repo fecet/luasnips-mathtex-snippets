daf4db828923ee00b8459d2fec7fa4a23c3c7129
fecet <xiezej@gmail.com>
2022-11-14 17:34:35 +0800

    fix space

--- lua/luasnip-latex-snippets/math_iA_no_backslash.lua
+++ lua/luasnip-latex-snippets/math_iA_no_backslash.lua
@@ -4,10 +4,10 @@ local with_priority = require("luasnip-latex-snippets.util.utils").with_priority
 local math_iA_no_backslash = {
 	ls.parser.parse_snippet({ trig = "sq", name = "\\sqrt{}" }, "\\sqrt{${1:${TM_SELECTED_TEXT}}} $0"),
 
-	with_priority(ls.parser.parse_snippet({ trig = "hat", name = "hat" }, "\\hat{$1}$0 "), 10),
-	with_priority(ls.parser.parse_snippet({ trig = "bar", name = "bar" }, "\\overline{$1}$0 "), 10),
-	with_priority(ls.parser.parse_snippet({ trig = "cal", name = "cal" }, "\\mathcal{$1}$0 "), 10),
-	with_priority(ls.parser.parse_snippet({ trig = "bf", name = "mathbf" }, "\\mathbf{$1}$0 "), 10),
+	with_priority(ls.parser.parse_snippet({ trig = "hat", name = "hat" }, "\\hat{$1}$0"), 10),
+	with_priority(ls.parser.parse_snippet({ trig = "bar", name = "bar" }, "\\overline{$1}$0"), 10),
+	with_priority(ls.parser.parse_snippet({ trig = "cal", name = "cal" }, "\\mathcal{$1}$0"), 10),
+	with_priority(ls.parser.parse_snippet({ trig = "bf", name = "mathbf" }, "\\mathbf{$1}$0"), 10),
 
 	ls.parser.parse_snippet({ trig = "inf", name = "\\infty" }, "\\infty"),
 	ls.parser.parse_snippet({ trig = "inn", name = "in " }, "\\in "),
